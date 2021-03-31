343c7b64ee7886709305c6b1c5f306b37f4fdfd3
Deepika Bekal <deepika.bekal2@gmail.com>
2021-03-31 15:44:50 +0400

    current weather API call and current weather display function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-var apiKey = "8534a9d4f88c86dec2955f7c2fa24a0f";
+var apiKey = "9734ad0baca8a3d2db8c9b1ab54c7751";
 var apiUrl = `https://api.openweathermap.org/data/2.5/weather`
+var units = "metric";
 var currentDate = "";
 var cityName = "";
 var uvValue = "";
@@ -7,22 +8,35 @@ var uvValue = "";
 $("#search-btn").click(function(){
     
     cityName = $("#search-city").val().trim();
-    
-
-      fetch(apiUrl+`?q=${cityName}&appid=${apiKey}`)
-          .then(function(response){
-              response.json().then(function(data){
-              console.log(data);
-              geoCordinates(data);
-              //currentWeather(data);
-      })
-  })
-
+    currentWeather(cityName);
 
 });
 
+function currentWeather(city){
+    
+    fetch(apiUrl+`?q=${city}&units=${units}&appid=${apiKey}`)
+            .then(function(response){
+             return response.json()
+            }) 
+            .then(function(data){
+              console.log(data);
+              getWeatherDetails(data);
+            })
 
+}
 
+function getWeatherDetails(info){
+    
+    $(".current-weather").append(
+        `
+        <p>Date: ${currentDate} </p>        
+        <p>Temperature: ${info.main.temp}&#8457</p>
+        <p>Humidity: ${info.main.humidity} %</p>
+        <p>Wind Speed: ${info.wind.speed} MPH</p>
+       
 
+        `
+    )
+}
 
  
