09c21c4c3c5ffd2ba1d14aad66d7ff61851bbd58
Deepika Bekal <deepika.bekal2@gmail.com>
2021-04-02 14:30:22 +0400

    5 day forecast function and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
 var apiKey = "9734ad0baca8a3d2db8c9b1ab54c7751";
 var apiUrl = `https://api.openweathermap.org/data/2.5/weather`
 var units = "metric";
-var currentDate = "";
-var cityName = "";
-var uvValue = "";
+var searchValues = [];
 
 //search button click
 $("#search-btn").click(function(){
     $(".current-weather").empty();
+    $(".forecast-container").empty();
     cityName = $("#search-city").val().trim();
     currentWeather(cityName);
 
@@ -31,6 +30,8 @@ function geoCordinates(cityN,currentData){
     console.log(cityN);
     var latitude = currentData.coord.lat;
     var longitude = currentData.coord.lon;
+    
+
     var api = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=${units}&exclude=minutely,hourly&appid=${apiKey}`
 
     fetch(api).then(function(response){
@@ -39,6 +40,7 @@ function geoCordinates(cityN,currentData){
         .then(function(data){
             console.log("uv",data);
             getWeatherDetails(cityN,data);
+            fiveDayForecast(cityN,data);
         })
 
 
@@ -48,8 +50,9 @@ function geoCordinates(cityN,currentData){
 //display current weather conditions
 function getWeatherDetails(city,info){
     var unixTimeStamp = info.current.dt;
-    var fullDate = new Date(unixTimeStamp*1000)
-    var currentDate = dayjs(fullDate).format("DD/MM/YYYY") ;
+    // var fullDate = new Date(unixTimeStamp*1000)
+    // var currentDate = dayjs(fullDate).format("DD/MM/YYYY") ;
+    var currentDate = dayjs.unix(unixTimeStamp).format("DD/MM/YYYY")
     console.log(currentDate);
     var uvIndex = info.current.uvi;
 
@@ -69,8 +72,8 @@ function getWeatherDetails(city,info){
     $(".current-weather").append(
         `
         <p>Date: ${currentDate} </p>        
-        <p>Temperature: ${info.current.temp}&#8457</p>
-        <p>Humidity: ${info.current.humidity} %</p>
+        <p>Temperature: ${info.current.temp}&#8451</p>
+        <p>Humidity: ${info.current.humidity}%</p>
         <p>Wind Speed: ${info.current.wind_speed} MPH</p>
         <p>UV Index: <span id="uv-color">${uvIndex}</span><p>
         `
@@ -89,6 +92,35 @@ function uvIndexColor(index){
     } else {
         $("#uv-color").attr("class","severe")
 
+    }
+}
+
+function fiveDayForecast(city, info){
+   
+    for(var i=1;i<6;i++){
+        
+        var unixTime = info.daily[i].dt
+        var dailyDate = dayjs.unix(unixTime).format("DD/MM/YYYY");
+        console.log(dailyDate);
+        var dailyIcon = info.daily[i].weather[0].icon;
+        var dailyTemp = info.daily[i].temp.day;
+        var dailyHumidity = info.daily[i].humidity;
+
+        $(".forecast-container").append(
+            `
+            <div class="col-sm-2 daily-container ${i}"></div>
+                       
+            `
+        )
+        
+        $(`.${i}`).append(
+            `
+            <h6>${dailyDate}</h6>
+            <img src=http://openweathermap.org/img/wn/${dailyIcon}@2x.png>
+            <p>Temp: ${dailyTemp}</p>
+            <p>Humidity: ${dailyHumidity} 
+            `
+        )
     }
 }
 
